e2c479754384008e14e1939ba0ac0a7e5664f22b
Lex Kalina <kalinalex91@gmail.com>
2017-05-22 21:49:28 +0300

    Docker support

---
+++ bin/wait_for_mysql.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+set -e
+
+host="$1"
+shift
+cmd="$@"
+
+args=(
+	-h $host
+	-u "root"
+	-D "sys"
+	--silent
+)
+
+
+until select="$(echo 'SELECT 1' | mysql "${args[@]}")" && [ "$select" == '1' ]; do
+  >&2 echo "MySQL is unavailable - sleeping"
+  sleep 1
+done
+
+>&2 echo "MySQL is up - executing command"
+eval $cmd
