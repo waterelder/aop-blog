0f1d31de6b405e151c2ad7a543b04164e65e762f
Alexander Dorozhkin <alex.d@evercodelab.com>
2017-06-02 19:05:27 +0300

    [CodeStyle] Fix codeStyle

--- src/AppBundle/Controller/RegistrationController.php
+++ src/AppBundle/Controller/RegistrationController.php
@@ -2,16 +2,22 @@
 
 namespace AppBundle\Controller;
 
-
+use AppBundle\Entity\User;
 use AppBundle\Form\UserType;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 class RegistrationController extends Controller
 {
     /**
      * @Route("/register", name="register")
+     *
+     * @param Request $request
+     *
+     * @return RedirectResponse|Response
      */
     public function registerAction(Request $request)
     {
@@ -21,14 +27,11 @@ class RegistrationController extends Controller
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
-
             $encoder = $this->get('security.password_encoder');
             $password = $encoder->encodePassword($user, $user->getPlainPassword());
             $user->setPassword($password);
 
-
             $user->setRole('ROLE_USER');
-
 
             $em = $this->getDoctrine()->getManager();
             $em->persist($user);
