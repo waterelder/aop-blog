b55ed9d2b11e1e30cc1f815603aa36176fdec006
Lex Kalina <kalinalex91@gmail.com>
2017-06-07 14:47:31 +0300

    [Docker] fix typo #3

--- bin/docker_afterdeploy.sh
+++ bin/docker_afterdeploy.sh
@@ -19,9 +19,7 @@ function post_deploy {
     run php bin/console cache:clear --no-debug --no-warmup --env="$1"
     run php bin/console doctrine:migrations:status --env="$1"
     run php bin/console doctrine:migrations:migrate --no-interaction --env="$1"
-    if  ["$1" == "dev"] || ["$1" == "test"] ;
-    then run php bin/console doctrine:fixtures:load --no-interaction --env="$1"
-    fi
+    run php bin/console doctrine:fixtures:load --no-interaction --env="$1"
     run chmod -R 777 web/uploads
     run chmod -R 777 var/logs
     run chmod -R 777 var/cache
